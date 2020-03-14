4060e1edf8d3a764afe62735976666ae993f2f4b
White <White@DESKTOP-ONL0M0Q>
2020-03-14 21:35:17 +0900

    1933 완료

--- 1933/src/Main.java
+++ 1933/src/Main.java
@@ -1,12 +1,9 @@
+import java.util.Scanner;
 
 public class Main {
 
 		public static void main(String[] args) {
-			System.out.println("설마 주석은안올라가니?");
-			System.out.println("설마 주석은안올라가니?");
-			System.out.println("설마 주석은안올라가니?");
-			System.out.println("설마 주석은안올라가니?");
-			System.out.println("설마 주석은안올라가니?");
+			
 			/*
 			 *   1 : 1
 			 *   2 : 1 2
@@ -35,6 +32,44 @@ public class Main {
 			 *  
 			 *  81 23
 			 */
+			
+			Scanner sc = new Scanner(System.in);
+			
+			// 1. 기본적인 풀이법
+			int num1 = sc.nextInt();
+			
+			
+		
+			for(int i=1 ; i*2 <= num1 ; i++)
+			{
+				if( num1 % i==0)
+				System.out.print(i+" " );
+				
+			
+			}
+			System.out.print(num1);
+			
+			
+//		
+//			
+//			// 2. 메모리 절약 풀이법 첫번째 약수는 기존 수의 반만 출력된다.
+//			int num2 = sc.nextInt();
+//			
+//			for(int i=1; i*i<=num2 ;i++)
+//			{
+//				if(num2%i==0)
+//				 {
+//					System.out.print(i+" " );
+//				 }
+//				
+//				if(i*i==num2)          //제곱수 확인
+//					{
+//					System.out.print("result"+i+" " );
+//					}
+//			}
+//		    
+			
+		
 		}
 }
 
